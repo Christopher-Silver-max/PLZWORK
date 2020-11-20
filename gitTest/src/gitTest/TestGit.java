cf27176bcb28e0a790c12a6f00108832eba891fb
csedm <csedm@10.100.39.18>
2020-11-20 11:58:26 -0500

    plz work

---
+++ gitTest/src/gitTest/TestGit.java
@@ -0,0 +1,17 @@
+package gitTest;
+
+import java.awt.EventQueue;
+
+//import databasetest.test;
+
+public class TestGit {
+
+	public static void main(String[] args)
+	{
+
+	System.out.println("in print");
+	}
+}
+		
+	
+
